dae55cc736daba36d0cb5389e8e170f712c4e159
Bran.HB <brandonhernandezbriones456@gmail.com>
2026-06-12 21:51:31 -0600

    Contador de clics individuales en react

--- mi-primer-react/src/App.jsx
+++ mi-primer-react/src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { useState } from 'react';
+
+export default function MyApp() {
   return (
     <div>
-      <h1>Hola, soy Brandon</h1>
-      <p>Estoy aprendiendo React</p>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
     </div>
-  )
+  );
 }
 
-export default App;
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
