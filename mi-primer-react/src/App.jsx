a06db45664c8a2c0649e973f84203555c49ffa9e
Bran.HB <brandonhernandezbriones456@gmail.com>
2026-06-25 12:41:09 -0600

    Provando el Frameword - Tailwind css para react

--- mi-primer-react/src/App.jsx
+++ mi-primer-react/src/App.jsx
@@ -1,31 +1,9 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-
-function Inicio() {
-  return <h1>Página de Inicio</h1>
-}
-
-function About() {
-  return <h1>Sobre mí</h1>
-}
-
-function Contacto() {
-  return <h1>Contacto</h1>
-}
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Inicio</Link> |{' '}
-        <Link to="/about">About</Link> |{' '}
-        <Link to="/contacto">Contacto</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-w-full h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-xl font-extrabold text-black">
+        Tailwind está funcionando! 🎉
+      </h1>
+    </div>
   )
 }
